0e3213821b996bd4b6e3704c6064ae4705bc5fbb
Riki Zulkarnain <rikyzulkarnain21gmail.com>
2026-02-15 16:53:35 +0700

    Fix Vercel env + port

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import { fileURLToPath } from 'url';
 import { GoogleGenAI } from '@google/genai';
 import dotenv from 'dotenv';
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
